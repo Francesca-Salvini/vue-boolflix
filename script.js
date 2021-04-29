707f16d67b71bf87103ecb0210d7324ab33448ef
Francesca Salvini <franci98sp@gmail.com>
2021-04-29 13:01:24 +0200

    creati input e button, impostata richiesta API nel script.js

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ var app = new Vue (
     {
 
         el: '#root',
-        data: {},
-        methods: {}
+        data: {
+            film : []
+        },
+        methods: {},
+        mounted() {
+            axios
+            .get('https://api.themoviedb.org/3/search/movie?', {
+                params : {
+                    api_key : '9e15066d27a78ef033c5f7eaffb30058', 
+                    language : 'it-IT',
+                    query : 'Rambo'
+
+                }
+            })
+            .then((response) => {
+                const result = response.data;
+                console.log(result);
+                this.film = result.response;
+                
+                
+                
+                
+            });
+            
+            
+            
+
+            
+        }
     });
