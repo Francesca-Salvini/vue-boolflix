0ccab2a89fd7d3cc137b6e226d17789fd9dde4ef
Francesca Salvini <franci98sp@gmail.com>
2021-04-29 16:43:04 +0200

    completata milestone 2

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var app = new Vue (
         el: '#root',
         data: {
             films : [],
-            userQuery : ''
+            userQuery : '',
+            series : []
         },
         methods: {
 
@@ -30,6 +31,27 @@ var app = new Vue (
                         
                         
                     });
+
+                    axios
+                        .get('https://api.themoviedb.org/3/search/tv?' , {
+                            params : {
+                                api_key : '9e15066d27a78ef033c5f7eaffb30058', 
+                                language : 'it-IT',
+                                query : this.userQuery
+                                
+                            }
+                        })
+                        .then((response) => {
+                            const result = response.data;
+                            // console.log(result);
+                            this.series = result.results;
+                            console.log(this.series);
+                            
+    
+                            
+                            
+                            
+                        });
             }
         },
         
